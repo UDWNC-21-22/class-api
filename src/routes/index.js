79d07904c4fcec7ba2955b8349b321b8170f6b16
ngdkhoi <ngdkhoi27600@gmail.com>
2021-11-10 09:46:18 +0700

    update version

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next)=>{
-    res.render('index', {title: "Express Js"})
+    res.render('index', {title: "Management Class API - version 1.1.0"})
 });
 
 
