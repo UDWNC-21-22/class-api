e15c996aa03b787cf0a8286d21c8512e5cd219e6
ngdkhoi <ngdkhoi27600@gmail.com>
2021-11-10 09:55:10 +0700

    test ci/c

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next)=>{
-    res.render('index', {title: "Management Class API - version 1.1.0"})
+    res.render('index', {title: "Management Class API - version 1.0.1"})
 });
 
 
