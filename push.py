8f6832c0cf3749d1ff8efc3a6a35089207a106a5
Nguyễn Đình Khôi <49549370+ngdkhoi@users.noreply.github.com>
2022-01-17 10:47:04 +0700

    Delete push.py

--- push.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-msg = input("Enter message commit: ")
-os.system("git config user.name ngdkhoi")
-os.system("git config user.email ngdkhoi27600@gmail.com")
-os.system("git add .")
-os.system("""git commit -m " """+msg+""" " """)
-os.system("git push origin main")
